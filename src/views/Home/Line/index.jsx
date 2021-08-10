d9514b79597c1471cb339b2354d0972d42bbb2aa
Jhonatan Uribe <jhonatan.uribe@Jhonatan-Uribe-MacBook-Pro.local>
2021-08-10 01:19:06 -0500

    Handling avatar loading errors

--- src/views/Home/Line/index.jsx
+++ src/views/Home/Line/index.jsx
@@ -1,10 +1,11 @@
 import styles from "./styles.css";
 import { PrimaryButton } from "src/buttons";
+import Avatar from "src/components/Avatar";
 
 export function Line({ person }) {
   return (
     <div className={styles.container}>
-      <img alt="User avatar" className={styles.avatar} src={person.avatarUrl} />
+      <Avatar className={styles.avatar} src={person.avatarUrl} />
       <div className={styles.name}>{person.name}</div>
       <PrimaryButton action={`/give/${encodeURIComponent(person.id)}`}>
         Give Feedback
